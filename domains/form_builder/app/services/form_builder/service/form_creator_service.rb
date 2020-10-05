bc81fff4ad5aea64ffd71483e40133a0f7fa1e46
Joshua Edwards <josh.dean.edwards@gmail.com>
2020-10-05 07:07:55 -0500

    removed leftover code

--- domains/form_builder/app/services/form_builder/service/form_creator_service.rb
+++ domains/form_builder/app/services/form_builder/service/form_creator_service.rb
@@ -4,12 +4,13 @@ module FormBuilder
   module Service
     # Saga to create forms
     class FormCreatorService < Base
+
       def new_form
-        FormBuilder::Action::Form::New(FormBuilder::Form).call
+        FormBuilder::Action::Form::New.new(FormBuilder::Repository::FormRepository(FormBuilder::Form)).call
       end
 
       def create_form(attrs)
-        FormBuilder::Action::Form::Create.new(FormBuilder::Form, attrs)
+        FormBuilder::Action::Form::Create.new(FormBuilder::Repository::FormRepository , FormBuilder::Form)
           .call({success: success, failure: failure}, attrs)
       end
     end
